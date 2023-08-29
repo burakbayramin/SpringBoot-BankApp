1164a3a4d41251958b906768cb05135ed7b9c5d1
burakbayramin <burakbaydevs@gmail.com>
2023-08-29 13:24:47 +0300

    models added

--- src/main/java/com/burakbayramin/bank/model/Account.java
+++ src/main/java/com/burakbayramin/bank/model/Account.java
@@ -24,4 +24,55 @@ public class Account {
 
     @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
     private Set<Transaction> transaction;
+
+    public Account() {
+    }
+
+    public Account(String id, BigDecimal balance, LocalDateTime creationDate, Customer customer, Set<Transaction> transaction) {
+        this.id = id;
+        this.balance = balance;
+        this.creationDate = creationDate;
+        this.customer = customer;
+        this.transaction = transaction;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Set<Transaction> getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Set<Transaction> transaction) {
+        this.transaction = transaction;
+    }
 }
--- src/main/java/com/burakbayramin/bank/model/Customer.java
+++ src/main/java/com/burakbayramin/bank/model/Customer.java
@@ -1,4 +1,22 @@
 package com.burakbayramin.bank.model;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private Set<Account> accounts;
+
+    
 }
